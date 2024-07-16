a918c21b57a3dd84abc9a33b92dc6facd810cb10
netshedzo <67120128+netshedz@users.noreply.github.com>
2024-07-16 21:22:48 +0200

    injecting javascript for date,time and city

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function updateTime() {
+  let johannesburgElement = document.querySelector("#Johannesburg");
+  let johannesburgDateElement = johannesburgElement.querySelector(".date");
+  let johannesburgTimeElement = johannesburgElement.querySelector(".time");
+  let johannesburgTime = moment().tz("Africa / Johannesburg");
 
+  johannesburgDateElement.innerHTML = johannesburgTime.format("MMMM Do YYYY");
+  johannesburgTimeElement.innerHTML = johannesburgTime.format(
+    "h:mm:ss[<small>]A[</small>]"
+  );
+  let lagosElement = document.querySelector("#lagos");
+  let lagosDateElement = lagosElement.querySelector(".date");
+  let lagosTimeElement = lagosElement.querySelector(".time");
+  let lagosTime = moment().tz("Africa/Lagos");
+
+  lagosDateElement.innerHTML = lagosTime.format("MMMM Do YYYY");
+  lagosTimeElement.innerHTML = lagosTime.format("h:mm:ss[<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
